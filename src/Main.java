a2e5fc75051013359b38979226db467b8fad4d9d
pengzhendong <275331498@qq.com>
2019-03-28 13:39:01 +0800

    Init algs4 repo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = StdIn.readLine();
+        StdOut.println("Hello " + name);
+    }
+
+}
